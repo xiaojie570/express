86d479145b7943d0aa5c7eab345d1d9bd458f97c
xiaojie570 <457732323@qq.com>
2017-04-06 15:59:40 +0800

    addtoken

--- routes/goods_flow.js
+++ routes/goods_flow.js
@@ -11,7 +11,21 @@ var goods_flowDao = require('../dao/goods_flowDao');
 
 //增加货物流动信息
 router.post('/addGoods_flow',function (req,res,next){
-    goods_flowDao.addGoods_flow(req,res,next);
+    if(!req.body.goods_id){
+        res.json({"status":"1"});//货物id为空
+    }else if(!req.body.loc_id){
+        res.json({"status":"2"});//储位为空
+    }else if(!req.body.car_id){
+        res.json({"status":"3"});//车辆为空
+    }else if(!req.body.type){
+        res.json({"status":"4"});//货物类型为空
+    }else if(!req.body.count){
+        res.json({"status":"5"});//货物数量为空
+    }else if(!req.body.money){
+        res.json({"status":"6"});//货物金额为空
+    }else {
+        goods_flowDao.addGoods_flow(req, res, next);
+    }
 });
 
 
