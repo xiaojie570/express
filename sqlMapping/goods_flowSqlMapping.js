7912279b7c3537e2c79b0020919d04f9cda6444f
xiaojie570 <457732323@qq.com>
2017-04-07 12:11:47 +0800

    add gods

--- sqlMapping/goods_flowSqlMapping.js
+++ sqlMapping/goods_flowSqlMapping.js
@@ -5,7 +5,8 @@ var goods_flow = {
     addGoods_flow:"insert into goods_flow(id,goods_id,loc_id,car_id,type,count,money,username) values(0,?,?,?,?,?,?,?)",
     queryInGoods:"select * from goods_flow where type=?",
     queryByUsername:"select * from goods_flow where username=?",
-    queryByUsernameAndOut:"select * from goods_flow where username=? and type=?"
+    queryByUsernameAndOut:"select * from goods_flow where username=? and type=?",
+    queryAll:"select * from goods_flow"
 };
 
 module.exports = goods_flow;
